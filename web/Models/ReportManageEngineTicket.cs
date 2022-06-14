16607d43511e2520e6d86d029db8fb884ac486b6
Christopher Pickering <cpickering@rhc.net>
2022-06-14 13:47:53 -0500

    feat(service tickets): updated to all non-numberic ticket numbers
    closes #68

--- web/Models/ReportManageEngineTicket.cs
+++ web/Models/ReportManageEngineTicket.cs
@@ -6,7 +6,7 @@ namespace Atlas_Web.Models
     public partial class ReportManageEngineTicket
     {
         public int ManageEngineTicketsId { get; set; }
-        public int? TicketNumber { get; set; }
+        public string TicketNumber { get; set; }
         public string Description { get; set; }
         public int? ReportObjectId { get; set; }
         public string TicketUrl { get; set; }
