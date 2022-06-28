d5800fe4d48dceb51e0e7225fae98416bb91dd0d
Christopher Pickering <cpickering@rhc.net>
2022-06-28 13:37:41 -0500

    refactor(css): removed unused styles

--- gulpfile.js/styles.js
+++ gulpfile.js/styles.js
@@ -41,4 +41,36 @@ gulp.task('css:build', function () {
     .pipe(gulp.dest('web/wwwroot/css/'));
 });
 
+gulp.task('css:rejected', function () {
+  var plugins = [postcssFocusWithin];
+  return gulp
+    .src('web/wwwroot/css/theme.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      purgecss({
+        content: [
+          'web/Pages/**/*.cshtml',
+          'web/wwwroot/js/**/*.js',
+          '!web/wwwroot/js/**/*build.js',
+          '!web/wwwroot/js/**/*min.js',
+          'web/wwwroot/lib/**/*.js',
+        ],
+        safelist: [
+          'breadcrumb',
+          'is-active',
+          'editor-liveEditorPrev',
+          'analytics-reviewed',
+          'epic-released',
+          'legacy',
+          'high-risk',
+          'self-service',
+          'analytics-certified',
+        ],
+        rejected: true,
+      }),
+    )
+    .pipe(rename('.rejected'))
+    .pipe(gulp.dest('web/wwwroot/css/'));
+});
+
 gulp.task('styles', gulp.parallel('css:build'));
