4345f416fb1a94e19176c67e18a01523ad4eaa0b
Christopher Pickering <cpickering@rhc.net>
2022-07-12 14:55:38 -0500

    feat(initiative): added option to hide an initiative from search
    closes #205

--- web/Pages/Initiatives/Edit.cshtml.cs
+++ web/Pages/Initiatives/Edit.cshtml.cs
@@ -96,6 +96,7 @@ namespace Atlas_Web.Pages.Initiatives
             NewInitiative.OperationOwnerId = Initiative.OperationOwnerId;
             NewInitiative.ExecutiveOwnerId = Initiative.ExecutiveOwnerId;
             NewInitiative.FinancialImpact = Initiative.FinancialImpact;
+            NewInitiative.Hidden = Initiative.Hidden;
             NewInitiative.StrategicImportance = Initiative.StrategicImportance;
 
             _context.Attach(NewInitiative).State = EntityState.Modified;
