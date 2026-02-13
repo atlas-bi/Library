c5da31a9ece95c75c1b5396c0dd3dd595632c46f
Christopher Pickering <christopher@landandsea.ch>
2026-02-13 09:27:06 -0600

    updated manage engine logging

--- web/Pages/Requests/Index.cshtml.cs
+++ web/Pages/Requests/Index.cshtml.cs
@@ -1,6 +1,5 @@
 using System.Text;
-using System.Xml;
-using System.Xml.Linq;
+using System.Text.Json;
 using Atlas_Web.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,83 +9,35 @@ namespace Atlas_Web.Pages.Requests
     public class IndexModel : PageModel
     {
         private readonly IConfiguration _config;
+        private readonly HttpClient _client;
 
         public IndexModel(IConfiguration config)
         {
             _config = config;
-        }
 
-        public void OnGet()
-        {
-            // api is never directly accessed
-        }
-
-        public class ApiResponse
-        {
-            public ApiResponse(string xml)
+            var handler = new HttpClientHandler
             {
-                // Constructor takes an xml string and converts to ApiResponse obj
-                var xdoc = XDocument.Parse(xml);
-                var result = xdoc.Descendants("result");
-                foreach (var r in result.Elements())
-                {
-                    if (r.Name.LocalName.Equals("status"))
-                    {
-                        this.Status = r.Value;
-                    }
-                    else if (r.Name.LocalName.Equals("message"))
-                    {
-                        this.Message = r.Value;
-                    }
-                }
-                var dictList = new List<Dictionary<string, string>>();
-                if (xdoc.Descendants("operation").First().Attribute("name").Value != "GET_REQUESTS")
-                // (note the plural)
-                {
-                    // a "Details" request will only have one dict to pass back, and doesn't have a "record" element
-                    dictList.Add(
-                        xdoc.Descendants("parameter")
-                            .ToDictionary(
-                                d => d.Element("name").Value,
-                                d => d.Element("value").Value
-                            )
-                    );
-                    this.Details = dictList;
-                }
-                else
-                {
-                    foreach (var record in xdoc.Descendants("record"))
-                    {
-                        var dict = record
-                            .Descendants("parameter")
-                            .ToDictionary(
-                                d => d.Element("name").Value,
-                                d => d.Element("value").Value
-                            );
+                ServerCertificateCustomValidationCallback = (_, _, _, _) => true,
+            };
 
-                        dict.Add("URL", record.Attribute("URI").Value);
-                        dictList.Add(dict);
-                        this.Details = dictList;
-                    }
-                }
-            }
-
-            public string Status { get; set; }
-            public string Message { get; set; }
-            public List<Dictionary<string, string>> Details { get; set; }
-        }
-
-        public class Utf8StringWriter : StringWriter
-        {
-            // it's possible the default UTF-16 encoding from the .NET stringwriter is screwing up the XML parser on ME???
-            // use UTF-8 instead, which is the default for network stuff
-            // http://stackoverflow.com/questions/25730816/how-to-return-xml-as-utf-8-instead-of-utf-16
-            // Use UTF8 encoding but write no BOM to the wire
-            public override Encoding Encoding
+            _client = new HttpClient(handler)
             {
-                get { return new UTF8Encoding(false); }
-            }
+                DefaultRequestVersion = new Version(1, 1),
+                DefaultVersionPolicy = HttpVersionPolicy.RequestVersionExact,
+            };
+
+            _client.DefaultRequestHeaders.Add("Accept", "application/vnd.manageengine.sdp.v3+json");
+            _client.DefaultRequestHeaders.Add(
+                "authtoken",
+                _config["AppSettings:manage_engine_tech_key"]
+            );
         }
+
+        public void OnGet() { }
+
+        // ---------------------------
+        // PUBLIC ACTIONS
+        // ---------------------------
 
         public async Task<ActionResult> OnPostAccessRequest(
             string reportName,
@@ -94,104 +45,19 @@ namespace Atlas_Web.Pages.Requests
             string directorName
         )
         {
-            string Result; // <--- Make sure this matches below
-            string xmlString;
-
-            // Build XML payload
-            using (var stringWriter = new Utf8StringWriter())
-            {
-                using var xmlWriter = new XmlTextWriter(stringWriter)
+            var request = BuildBaseRequest(
+                subject: "Atlas Access Request",
+                description: $"I would like access to '{reportName}' from {reportUrl}",
+                itemName: "Request Access",
+                udfFields: new
                 {
-                    Formatting = Formatting.Indented,
-                };
-                xmlWriter.WriteStartDocument();
-                xmlWriter.WriteStartElement("Operation");
-                xmlWriter.WriteStartElement("Details");
-
-                void WriteParameter(string name, string value)
-                {
-                    xmlWriter.WriteStartElement("parameter");
-                    xmlWriter.WriteElementString("name", name);
-                    xmlWriter.WriteElementString("value", value);
-                    xmlWriter.WriteEndElement();
+                    udf_sline_4517 = directorName,
+                    udf_sline_5791 = reportName,
+                    udf_sline_5790 = reportUrl,
                 }
+            );
 
-                WriteParameter("requesttemplate", "WebAPI");
-                WriteParameter("subject", "Atlas Access Request");
-                WriteParameter(
-                    "description",
-                    $"I would like access to the report '{reportName}' from {reportUrl}"
-                );
-                WriteParameter("Atlas Link", reportUrl);
-                WriteParameter("category", "Epic Request");
-                WriteParameter("requester", User.GetUserName());
-                WriteParameter("item", "Request Access");
-                WriteParameter("subcategory", "Atlas");
-                WriteParameter("report_name", reportName);
-                WriteParameter("requesteremail", User.GetUserEmail());
-                WriteParameter("director", directorName);
-
-                xmlWriter.WriteEndElement(); // Details
-                xmlWriter.WriteEndElement(); // Operation
-                xmlWriter.WriteEndDocument();
-
-                xmlString = stringWriter.ToString();
-            }
-
-            // HttpClient ignoring SSL errors
-            var handler = new HttpClientHandler
-            {
-                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true,
-            };
-
-            using var client = new HttpClient(handler);
-            var values = new Dictionary<string, string>
-            {
-                { "OPERATION_NAME", "ADD_REQUEST" },
-                { "TECHNICIAN_KEY", _config["AppSettings:manage_engine_tech_key"] },
-                { "INPUT_DATA", xmlString },
-            };
-
-            var httpRequest = new FormUrlEncodedContent(values);
-
-            HttpResponseMessage httpResponse;
-            try
-            {
-                httpResponse = await client.PostAsync(
-                    _config["AppSettings:manage_engine_server"] + "/sdpapi/request/",
-                    httpRequest
-                );
-            }
-            catch (Exception ex)
-            {
-                return Content($"HTTP request failed: {ex.Message}");
-            }
-
-            string responseText = await httpResponse.Content.ReadAsStringAsync();
-
-            // DEBUG: log the response
-            Console.WriteLine("ManageEngine Response:");
-            Console.WriteLine(responseText);
-
-            // Try parse XML safely
-            try
-            {
-                if (responseText.TrimStart().StartsWith("<"))
-                {
-                    ApiResponse response = new(responseText);
-                    Result = $"{response.Status} - {response.Message}";
-                }
-                else
-                {
-                    Result = $"Server returned non-XML response:\n{responseText}";
-                }
-            }
-            catch (XmlException ex)
-            {
-                Result = $"Invalid XML received: {ex.Message}\n{responseText}";
-            }
-
-            return Content(Result);
+            return await SendRequestAsync(request);
         }
 
         public async Task<ActionResult> OnPostShareFeedback(
@@ -200,104 +66,67 @@ namespace Atlas_Web.Pages.Requests
             string description
         )
         {
-            string Result;
-            string xmlString;
+            var request = BuildBaseRequest(
+                subject: "Atlas Feedback",
+                description: $"<b>Atlas feedback on <a href='{reportUrl}'>{reportName}</a></b><br/><br/><p>{description}</p>",
+                itemName: "Feedback",
+                udfFields: new { udf_sline_5791 = reportName, udf_sline_5790 = reportUrl }
+            );
 
-            using (var stringWriter = new Utf8StringWriter())
-            {
-                using var xmlWriter = new XmlTextWriter(stringWriter);
-                xmlWriter.Formatting = System.Xml.Formatting.Indented;
-                xmlWriter.WriteStartDocument();
-                xmlWriter.WriteStartElement("Operation");
-                xmlWriter.WriteStartElement("Details");
-                xmlWriter.WriteStartElement("parameter");
-                xmlWriter.WriteElementString("name", "requesttemplate");
-                xmlWriter.WriteElementString("value", "WebAPI");
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteStartElement("parameter");
-                xmlWriter.WriteElementString("name", "subject");
-                xmlWriter.WriteElementString("value", "Atlas Feedback");
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteStartElement("parameter");
-                xmlWriter.WriteElementString("name", "purpose");
-                xmlWriter.WriteElementString(
-                    "value",
-                    "Atlas feedback reported on '" + reportName + "' from " + reportUrl
-                );
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteStartElement("parameter");
-                xmlWriter.WriteElementString("name", "Atlas Link");
-                xmlWriter.WriteElementString("value", reportUrl);
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteStartElement("parameter");
-                xmlWriter.WriteElementString("name", "category");
-                xmlWriter.WriteElementString("value", "Epic Request");
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteStartElement("parameter");
-                xmlWriter.WriteElementString("name", "description");
-                xmlWriter.WriteElementString("value", description);
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteStartElement("parameter");
-                xmlWriter.WriteElementString("name", "requester");
-                xmlWriter.WriteElementString("value", User.GetUserName());
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteStartElement("parameter");
-                xmlWriter.WriteElementString("name", "item");
-                xmlWriter.WriteElementString("value", "Problem/Issue");
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteStartElement("parameter");
-                xmlWriter.WriteElementString("name", "subcategory");
-                xmlWriter.WriteElementString("value", "Atlas");
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteStartElement("parameter");
-                xmlWriter.WriteElementString("name", "report_name");
-                xmlWriter.WriteElementString("value", reportName);
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteStartElement("parameter");
-                xmlWriter.WriteElementString("name", "responsibility");
-                xmlWriter.WriteElementString("value", "YES");
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteStartElement("parameter");
-                xmlWriter.WriteElementString("name", "requesteremail");
-                xmlWriter.WriteElementString("value", User.GetUserEmail());
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteStartElement("parameter");
-                xmlWriter.WriteElementString("name", "director");
-                xmlWriter.WriteElementString("value", "dryan@rhc.net");
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteStartElement("parameter");
-                xmlWriter.WriteElementString("name", "responsible_person");
-                xmlWriter.WriteElementString("value", "dryan@rhc.net");
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteEndElement();
-                xmlWriter.WriteEndDocument();
-                // Spit out the XML document
-                xmlString = stringWriter.ToString();
-            }
+            return await SendRequestAsync(request);
+        }
 
-            using (var client = new HttpClient())
+        // ---------------------------
+        // SHARED HELPERS
+        // ---------------------------
+
+        private object BuildBaseRequest(
+            string subject,
+            string description,
+            string itemName,
+            object udfFields
+        )
+        {
+            return new
             {
-                // Compose the http POST request following the API guidelines
-                // https://www.manageengine.com/products/service-desk/help/adminguide/api/request-operations.html#add
-                var values = new Dictionary<string, string>
+                request = new
                 {
-                    { "OPERATION_NAME", "ADD_REQUEST" },
-                    { "TECHNICIAN_KEY", _config["AppSettings:manage_engine_tech_key"] },
-                    { "INPUT_DATA", xmlString },
-                };
+                    subject,
+                    description,
+                    requester = new { email_id = User.GetUserEmail() },
+                    template = new { name = "WebAPI" },
+                    status = new { name = "Open" },
+                    category = new { name = "Epic Request" },
+                    subcategory = new { name = "Atlas" },
+                    item = new { name = itemName },
+                    udf_fields = udfFields,
+                },
+            };
+        }
 
-                var httpRequest = new FormUrlEncodedContent(values);
-                // send the request asynchronously
-                var httpResponse = await client.PostAsync(
-                    _config["AppSettings:manage_engine_server"] + "/sdpapi/request/",
-                    httpRequest
+        private async Task<ActionResult> SendRequestAsync(object payload)
+        {
+            var json = JsonSerializer.Serialize(payload);
+            var content = new FormUrlEncodedContent(
+                new Dictionary<string, string> { { "input_data", json } }
+            );
+
+            var url = _config["AppSettings:manage_engine_server"] + "/api/v3/requests";
+
+            try
+            {
+                var response = await _client.PostAsync(url, content);
+                var body = await response.Content.ReadAsStringAsync();
+
+                return Content(
+                    $"Status Code: {(int)response.StatusCode}\nResponse Body:\n{body}",
+                    "application/json"
                 );
-                // notify the user of the response from the server http://stackoverflow.com/a/13550295/3900824
-                ApiResponse response = new(await httpResponse.Content.ReadAsStringAsync());
-                Result = response.Status + " - " + response.Message;
             }
-            return Content(Result);
+            catch (Exception ex)
+            {
+                return Content($"HTTP request failed: {ex}");
+            }
         }
     }
 }
--- web/wwwroot/js/polyfill/focus-within.js
+++ web/wwwroot/js/polyfill/focus-within.js
@@ -8,17 +8,17 @@ function focusWithin(e, t) {
   } catch (e) {}
   function s() {
     for (var t; (t = c.pop()); )
-      r && t.removeAttribute(r), n && t.classList.remove(n);
+      (r && t.removeAttribute(r), n && t.classList.remove(n));
     var a = e.activeElement;
     if (!/^(#document|HTML|BODY)$/.test(Object(a).nodeName))
       for (; a && 1 === a.nodeType; )
-        r && a.setAttribute(r, ''),
+        (r && a.setAttribute(r, ''),
           n && a.classList.add(n),
           c.push(a),
-          (a = a.parentNode);
+          (a = a.parentNode));
   }
   function i() {
-    e.addEventListener('focus', s, !0), e.addEventListener('blur', s, !0);
+    (e.addEventListener('focus', s, !0), e.addEventListener('blur', s, !0));
   }
   return (
     (function t() {
